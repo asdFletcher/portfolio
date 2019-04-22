e9d94dfc222ddb5c2224cea9c97a6106009ac9f4
Fletcher LaRue <laruefletcher@gmail.com>
2019-04-22 01:24:12 -0700

    filler for resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+const md = 
+`
+#Fletcher LaRue
+About: 
+
+- [Linkedin](https://www.linkedin.com/in/fletcher-larue/)
+- [Github](https://github.com/asdFletcher)
+- [Twitter](https://twitter.com/asdFletch)
+`
+
 const Resume = () => {
   return (
-    <div> Resume </div>
+    <div className="resume-container" >
+      <a href="https://www.linkedin.com/in/fletcher-larue/" target="_blank">Linkedin</a>
+    </div>
   )
 }
 
