6be96177f4f539283ebb3d04033ec704f200260a
Fletcher LaRue <laruefletcher@gmail.com>
2019-07-30 21:52:23 -0700

    fixes spelling error

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ Full Stack JavaScript developer with a long passion for coding, problem solving,
 
 Experience with Node.js, Express servers, React and React Native apps, and creating APIs and databases. Currently working on a variety of projects, networking, and studying data structures 👍!
 
-My background is in Mechanical Engineering ⚙️, and I've developed a full suite of skills that fit in the aerospace world including mechanical design, expert CAD skills, and project management 📞. Now  that I've seen the light and switched to web development it's possible some of these skills trasfer, like working with a team on technical challenges! 
+My background is in Mechanical Engineering ⚙️, and I've developed a full suite of skills that fit in the aerospace world including mechanical design, expert CAD skills, and project management 📞. Now  that I've seen the light and switched to web development it's possible some of these skills transfer, like working with a team on technical challenges! 
 `
 
 const Home = () => {
