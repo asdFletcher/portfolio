941f324599f3b06358e7844c8c5d2f68cc538d75
Fletcher LaRue <laruefletcher@gmail.com>
2020-08-16 19:23:43 -0700

    reword home page
    reword homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import React from "react";
+import ReactMarkdown from "react-markdown";
 
-const md = 
-`
+const md = `
 #### Fletcher LaRue
 About: 
 <ul>
@@ -15,17 +14,19 @@ About:
 
 Full Stack JavaScript developer with a long passion for coding, problem solving, and building cool stuff!
 
-Experience with Node.js, Express servers, React and React Native apps, and creating APIs and databases. Currently working on a variety of projects, networking, and studying data structures 👍!
+I can handle both front end and backend programming and I am adatable to whichever language the project is using. I have experience building React web apps and backend services in Node, Python and Java. I also have experience building desktop C# apps! I know how to design APIs and integrate services to databases, as well as model tables and relationships. I can also create one-click CI/CD pipelines using a bunch of AWS tools, including AWS CloudFormation template, which makes the entire stack and pipeline replicable for multiple environments, allows for quick teardown and replacement, and versioning comes included if the templates are tracked via Git!
 
-My background is in Mechanical Engineering ⚙️, and I've developed a full suite of skills that fit in the aerospace world including mechanical design, expert CAD skills, and project management 📞. Now  that I've seen the light and switched to web development it's possible some of these skills transfer, like working with a team on technical challenges! 
-`
+Currently working to better the world at a San Diego startup Cardea Bio, where we add biology to a graphene layer and get electrical signals out, thus revealing information nobody has had access to before!
+
+Check out my [linkedin](https://www.linkedin.com/in/fletcher-larue/) for more insights into my work history, or feel free to reach out directly!
+`;
 
 const Home = () => {
-  return (
-    <div className="home-container">
-      <ReactMarkdown source={md} escapeHtml={false}/>
-    </div>
-  );
-}
+    return (
+        <div className="home-container">
+            <ReactMarkdown source={md} escapeHtml={false} />
+        </div>
+    );
+};
 
 export default Home;
